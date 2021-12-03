05bcb0e32123c2ac996c0871e8c010cdd4ce5273
phil <philippe.tedajo@gmail.com>
2021-12-03 14:24:32 +0100

    update

--- src/services/screenShoot.ts
+++ src/services/screenShoot.ts
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 import cloudinary from "./cloudinary";
 
 export const screenShoot = async (id: string) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   // set params here before open headless browser
   await page.goto(`http://localhost:3000/live-preview/${id}`);
